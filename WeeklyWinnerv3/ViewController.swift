85ca94a556802ea7df229b5c881cfc28385d6962
Reid Brown <56902716+reidbrown1@users.noreply.github.com>
2022-06-24 20:07:53 -0400

    Initial Commit

---
+++ WeeklyWinnerv3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyWinnerv3
+//
+//  Created by Reid Brown on 6/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
